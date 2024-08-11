2cb94c38ec924455ca4dab1e12766818f6efc129
Ashutosh Chauhan <100701424+theashu02@users.noreply.github.com>
2024-08-12 02:22:46 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -15,7 +15,7 @@ function Login() {
   const handleGoogleLogin = () => {
     // Redirect to Google login URL
     window.location.href =
-      "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:5173/";
+      "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reach-in-box-lovat.vercel.app/";
   };
 
   return (
